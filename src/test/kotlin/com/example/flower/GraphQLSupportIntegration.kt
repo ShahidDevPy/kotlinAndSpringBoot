65d674622cfbd33df2de99f66c0683765bb83c8d
Shahid Rasool <shahidrasool.dev@gmail.com>
2023-07-31 07:45:19 +0500

    Removed unused imports

--- src/test/kotlin/com/example/flower/GraphQLSupportIntegration.kt
+++ src/test/kotlin/com/example/flower/GraphQLSupportIntegration.kt
@@ -5,15 +5,12 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
-import org.springframework.http.HttpStatus
 
 @SpringBootTest
 class GraphQLSupportIntegration(@Autowired val graphQLTest: GraphQLTest ) {
     @Test
     fun `test hello` () {
-        val controller = ApiController();
+        val controller = ApiController()
         assertEquals("hello, My name is Shahid Rasool", controller.hello())
 
     }
